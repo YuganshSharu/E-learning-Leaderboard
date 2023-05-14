f4d5cb5931cdc06d6fe3c8206b6ee4d31c5527ed
Yugansh Sharu <yugansh19347@iiitd.ac.in>
2023-05-15 00:46:54 +0530

    kjlk

--- E-learning-Frontend/src/Components/Leaderboard/Badge.js
+++ E-learning-Frontend/src/Components/Leaderboard/Badge.js
@@ -1,15 +1,18 @@
-import { ReactComponent as GoldBadge } from '../../Assets/Images/gold.svg';
-import { ReactComponent as SilverBadge } from '../../Assets/Images/silver.svg';
-import { ReactComponent as BronzeBadge } from '../../Assets/Images/bronze.svg';
+// import { ReactComponent as GoldBadge } from '../../Assets/Images/gold.svg';
+// import { ReactComponent as SilverBadge } from '../../Assets/Images/silver.svg';
+// import { ReactComponent as BronzeBadge } from '../../Assets/Images/bronze.svg';
+import GoldBadge from '../../Assets/Images/gold.png';
+import SilverBadge from '../../Assets/Images/silver.png';
+import BronzeBadge from '../../Assets/Images/bronze.png';
 const Badge = (props) => {
   const badgeStyle = { width: '20px', height: 'auto' };
 
   return props.badgeName === 'gold' ? (
-    <GoldBadge style={badgeStyle} />
+    <img src={GoldBadge} style={badgeStyle} />
   ) : props.badgeName === 'silver' ? (
-    <SilverBadge style={badgeStyle} />
+    <img src={SilverBadge} style={badgeStyle} />
   ) : (
-    <BronzeBadge style={badgeStyle} />
+    <img src={BronzeBadge} style={badgeStyle} />
   );
 };
 export default Badge;
